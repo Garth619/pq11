73cb56e3a43c3287d913a3dcef2a28dc46310533
Garrett Cullen <Garth619@users.noreply.github.com>
2020-06-26 13:33:58 -0700

    push

--- wp-content/themes/pq11/page-templates/includes/homepage_template_parts/section-1.php
+++ wp-content/themes/pq11/page-templates/includes/homepage_template_parts/section-1.php
@@ -1,65 +1,63 @@
 <section id='section_one'>
 
-  <div id='sec_one_inner'>
+  <div id='sec_one_left'>
   
-    <div id='sec_one_content'>
+    <div id='sec_one_left_inner'>
     
-      <span id='sec_one_subtitle'><?php the_field( 'section_one_subtitle' ); ?></span><!-- sec_one_subtitle -->
+      <div id='sec_left_top'>
+      
+        <div id='video_wrapper'>
+        
+          <div id='sec_one_video'>
 
-      <span id='sec_one_title'><?php the_field( 'section_one_large_title' ); ?></span><!-- sec_one_title -->
+            <a href="">
+          
+              <img src='<?php bloginfo('template_directory');?>/images/vid.jpg' /><!-- name -->
 
-      <a class='button free_consultation' href='#consultation'><span><?php the_field( 'button_verbiage' ); ?></span></a><!-- button -->  
+            </a>
+
+            <div id='video_overlay'>
+            
+            <div class='play_button'></div><!-- play_button -->
+            
+            </div><!-- video_overlay -->
+
+          </div><!-- sec_one_video -->
+        
+        </div><!-- video_wrapper -->
+
+        <div id='sec_one_content'>
+            
+          <span id="sec_one_title">Fighting for our clients is our highest priority</span>
+
+          <ul id='sec_one_selling_points'>
+          
+            <li>60+ YEARS EXpERTISE</li>
+            <li>$500M+ IN RESULTS</li>
+            <li>10K+ CLIENTS SERVED</li>
+          
+          </ul><!-- sec_one_selling_points -->
+            
+        </div><!-- sec_one_content -->
+      
+      </div><!-- sec_left_top -->
+
+      <div id='sec_left_bottom'>
+      
+        
+      
+      </div><!-- sec_left_bottom -->
     
-    </div><!-- sec_one_content -->
+    </div><!-- sec_one_left_inner -->
+
+    <img id='hero' src='<?php bloginfo('template_directory');?>/images/hero-mobile-new.jpg' /><!-- hero -->
   
-  </div><!-- sec_one_inner -->
+  </div><!-- sec_one_left -->
 
-  <picture>
+  
 
-    <?php $hero_desktop_new_webp = get_field( 'hero_desktop_new_webp' ); ?>
-    
-    <?php if ( $hero_desktop_new_webp ) { ?>
-	  
-      <source media="(min-width: 1170px)" srcset="<?php echo $hero_desktop_new_webp['url']; ?>" type="image/webp">
-    
-    <?php } ?>
-    
-    <?php $hero_desktop_new = get_field( 'hero_desktop_new' ); ?>
-    
-    <?php if ( $hero_desktop_new ) { ?>
+  <div id='sec_one_right'>
+  
+  </div><!-- sec_one_right -->
 
-      <source media="(min-width: 1170px)" srcset="<?php echo $hero_desktop_new['url']; ?>">
-
-    <?php } ?>
-
-    <?php $hero_tablet_webp_new = get_field( 'hero_tablet_webp_new' ); ?>
-    
-    <?php if ( $hero_tablet_webp_new ) { ?>
-	    
-      <source media="(min-width: 768px)" srcset="<?php echo $hero_tablet_webp_new['url']; ?>" type="image/webp">
-      
-    <?php } ?>
-
-    <?php $hero_tablet_new = get_field( 'hero_tablet_new' ); ?>
-    
-    <?php if ( $hero_tablet_new ) { ?>
-	    
-      <source media="(min-width: 768px)" srcset="<?php echo $hero_tablet_new['url']; ?>">
-      
-    <?php } ?>
-
-    <?php $hero_mobile_webp_new = get_field( 'hero_mobile_webp_new' ); ?>
-    
-    <?php if ( $hero_mobile_webp_new ) { ?>
-	    
-      <source srcset="<?php echo $hero_mobile_webp_new['url']; ?>" type="image/webp">
-      
-    <?php } ?>
-
-    <?php $hero_mobile_new = get_field( 'hero_mobile_new' ); ?>
-
-    <img id='sec_one_hero' src='<?php echo $hero_mobile_new['url']; ?>' />
-
-  </picture>
-
-</section><!-- section_one -->
+</section><!-- sec_one -->
--- wp-content/themes/pq11/page-templates/template-home.php
+++ wp-content/themes/pq11/page-templates/template-home.php
@@ -4,8 +4,7 @@
 	
 	get_header();
 
-
-	// get_template_part( 'page-templates/includes/homepage_template_parts/section', '1' );
+	get_template_part( 'page-templates/includes/homepage_template_parts/section', '1' );
 	// get_template_part( 'page-templates/includes/homepage_template_parts/section', '2' );
 	// get_template_part( 'page-templates/includes/homepage_template_parts/section', '3' );
 	// get_template_part( 'page-templates/includes/homepage_template_parts/section', '4' );
