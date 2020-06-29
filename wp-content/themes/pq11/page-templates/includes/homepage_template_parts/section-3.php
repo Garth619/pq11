c5ec60573eea3f9ef300c7ea49c853478aa26c9a
Garrett Cullen <Garth619@users.noreply.github.com>
2020-06-29 16:30:01 -0700

    push

--- wp-content/themes/pq11/page-templates/includes/homepage_template_parts/section-3.php
+++ wp-content/themes/pq11/page-templates/includes/homepage_template_parts/section-3.php
@@ -1,56 +1,47 @@
-<section id="section_three">
+<section id='section_three'>
 
 	<div id='sec_three_inner'>
+	
+		<h1 id='sec_three_header'>Full Service Law Firm in Lansing & Surrounding Areas</h1><!-- sec_three_header -->
 
-		<div id='sec_three_left' class="content">
+		<span class='double_line'></span><!-- double_line -->
+
+		<div id='sec_three_intro' class="content">
 		
-			<h1 class='sec_three_header global_header'><?php the_field( 'section_three_header' ); ?></h1><!-- sec_three_header global_header -->
-
-			<div id='sec_three_content_inner'>
-			
-				<?php the_field( 'section_three_content' ); ?>
-
-			</div><!-- sec_three_content_inner -->
-
-		</div><!-- sec_three_left -->
-
-		<div id='sec_three_right'>
-
-		<?php if ( have_rows( 'section_three_attorneys' ) ) : ?>
-			<?php while ( have_rows( 'section_three_attorneys' ) ) : the_row(); ?>
+			<p>At White Law PLLC, we use a team approach to solve our clients’ legal issues, an approach that combines decades of shared insight with the use of advanced technology in the office and the courtroom. We know how to get our clients the results they need in the toughest legal arenas. We put our clients first and you will always work directly with your attorney throughout your case. Our innovative management system further streamlines our process and reduces the amount of natural resources we consume. We offer the cutting-edge services and solutions you deserve.</p>
 		
-				<div id="" class='sec_three_attorney'>
-			
-					<div class='attorney_image_wrapper'>
-
-						<?php $attorney_image = get_sub_field( 'attorney_image' ); ?>
-						
-						<?php if ( $attorney_image ) { ?>
-
-							<div class="att_image">
-						
-								<img class='attorney_image_one lazyload' data-src="<?php echo $attorney_image['url']; ?>" alt="<?php echo $attorney_image['alt']; ?>" />
-
-							</div><!-- att_image -->
-						
-						<?php } ?>
-
-						<span class='sec_three_att_title'><?php the_sub_field( 'attorney_name' ); ?></span><!-- sec_three_att_title -->
-
-						<span class='sec_three_att_subtitle'><?php the_sub_field( 'attorney_description' ); ?></span><!-- sec_three_att_subtitle -->
-
-						<span class='sec_three_att_quote'><?php the_sub_field( 'attorney_testimonial' ); ?></span><!-- sec_three_att_quote -->
-
-						<a class='sec_three_att_button button_two' href='<?php the_sub_field( 'attorney_button_link' ); ?>'><?php the_sub_field( 'attorney_button_verbiage' ); ?></a><!-- sec_three_att_button -->
-			
-					</div><!-- attorney_image_wrapper -->
+		</div><!-- sec_three_intro -->
 		
-				</div><!-- sec_three_attorney -->
-			
-			<?php endwhile; ?>
-		<?php endif; ?>
+		<div id='sec_three_col_wrapper' class="content">
 		
-		</div><!-- sec_three_right -->
+			<div class='sec_three_col'>
+		
+				<h2>The White Law Difference</h2>
+
+				<p>Sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+
+				<ul>
+					<li>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut</li>
+					<li>Aut fugit, sed quia consequuntur magni doloreseos qui ratione</li>
+					<li>Neque porro quisquam est, qui dolorem ipsum quia dolor sit consectetur, adipisci velit</li>
+					<li>Sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</li>
+				</ul>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Duis aute irure dolor in reprehenderit in voluptate velit. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Duis aute irure dolor in reprehenderit in voluptate velit.</p>
+		
+			</div><!-- sec_three_col -->
+
+			<div class='sec_three_col'>
+		
+				<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed.</p>
+
+				<h2>Occaecat cupidatat non proident ur sint occaecat cupidatat non proident</h2>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Caspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+		
+			</div><!-- sec_three_col -->
+		
+		</div><!-- sec_three_col_wrapper -->
 
 	</div><!-- sec_three_inner -->
 
