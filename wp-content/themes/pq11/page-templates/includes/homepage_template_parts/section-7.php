35dbb49d89d7857756326dfe704f067db3e212aa
Garrett Cullen <Garth619@users.noreply.github.com>
2020-06-30 19:21:42 -0700

    push

--- wp-content/themes/pq11/page-templates/includes/homepage_template_parts/section-7.php
+++ wp-content/themes/pq11/page-templates/includes/homepage_template_parts/section-7.php
@@ -7,6 +7,8 @@
     <span id='sec_seven_title'>Practice Areas</span><!-- sec_seven_title -->
 
     <span class='double_line'></span><!-- double_line -->
+
+    <a class='button_two' href=''>View All Practice Areas</a><!-- button_two -->
   
   </div><!-- sec_seven_left -->
 
@@ -19,6 +21,12 @@
       <li><a href="">Bad Faith Insurance</a></li>
       <li><a href="">Construction Accidents</a></li>
       <li><a href="">Premises Liability</a></li>
+      <li><a href="">Dog Bites and Attacks</a></li>
+      <li><a href="">Explosion Accidents</a></li>
+      <li><a href="">Product Liability</a></li>
+      <li><a href="">Roundup Lawsuits</a></li>
+      <li><a href="">Wrongful Death Accidents</a></li>
+      <li><a href="">Slip and Fall Accidnts</a></li>
     </ul>
 
     <a class='button_two' href=''>View All Practice Areas</a><!-- button_two -->
