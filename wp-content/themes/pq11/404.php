c6e1c83f02e7de032716bd28cb0aacb32c0d2d35
Garrett Cullen <Garth619@users.noreply.github.com>
2020-07-07 13:27:48 -0700

    push

--- wp-content/themes/pq11/404.php
+++ wp-content/themes/pq11/404.php
@@ -1,10 +1,27 @@
 <?php get_header(); ?>
 
+<div id="internal_main">
+	
+	<div class="page_container one_col">
 
+		<div id='not_found_wrapper' class="internal_wrapper">
+		
+			<h1 id='internal_header'><?php the_field( 'not_found_title','option'); ?></h1><!-- internal_header -->
+
+			<span class='double_line'></span><!-- double_line -->
+
+			<span id='not_found_subtitle'><?php the_field( 'not_found_subtitle','option'); ?></span><!-- not_found_subtitle -->
+
+			<div id='not_found_content' class="content">
 			
-				<span class='go_back'>Go back</span><!-- button_two -->
+				<?php the_field( 'not_found_content','option'); ?>
+			
+			</div><!-- not_found_content -->
 
-				
-
-
+		</div><!-- not_found_wrapper -->
+		
+	</div><!-- page_container -->
+	
+</div><!-- internal_main -->
+			
 <?php get_footer(); ?>
