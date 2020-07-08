fb6043cf00d43521db6cc750fb2d62aa5f5ce759
Garrett Cullen <Garth619@users.noreply.github.com>
2020-07-08 13:02:18 -0700

    push

--- wp-content/themes/pq11/page-templates/template-testimonials.php
+++ wp-content/themes/pq11/page-templates/template-testimonials.php
@@ -12,23 +12,31 @@ get_header(); ?>
 
 		<div id='testimonials_wrapper' class="internal_wrapper">
 
-			<div class='single_test'>
-			
-				<?php echo file_get_contents( get_template_directory() . '/images/stars.svg' ); ?>
-
-				<span class='single_test_descrip'>I am indebted to Mr. White and White Law for the rest of my life. They are like family to me.</span><!-- single_test_descrip -->
-
-				<div class='single_test_content content'>
+			<?php if ( have_rows( 'testi_page' ) ) : ?>
 				
-					<p>People v. F.T. (2009) - “When my case started, it looked like my life was over with. I didn’t know what to do. I was charged with first degree criminal sexual assault and six counts of rape; I was facing prison for the rest of my life. I went to jail, my children were taken from me… I didn’t know what to do. ​ A friend of mine recommended White Law PLLC and attorney James White. They jumped right into my case and my situation. Mr. White opened my eyes and shined the light on my case; they did everything in their power to help me. They hired a private investigator and expert witnesses. The last week of my trial, I was in the office almost every day; I know that Mr. White put his heart and soul into my trial, and that he believed in me. ​ When my trial began, everyone that I saw would look at me funny, like I was a bad person. By the time the witnesses had finished testifying at trial, everyone was using those bad looks for my accusers. I was found not guilty and cleared of all charges. ​ God helped me through White Law PLLC, and I am indebted to Mr. White and White Law for the rest of my life. They are like family to me. If you’re in trouble and you think your life is over, call White Law. They gave me my wife, my family, and my life back.”</p>
-				
-				</div><!-- single_test_content -->
+				<?php while ( have_rows( 'testi_page' ) ) : the_row(); ?>
 
-				<span class='double_line'></span><!-- double_line -->
-
-				<span class='single_test_name'>First Last Name</span><!-- single_test_name -->
+					<div class='single_test'>
 			
-			</div><!-- single_test -->
+						<?php echo file_get_contents( get_template_directory() . '/images/stars.svg' ); ?>
+
+						<span class='single_test_descrip'><?php the_sub_field( 'intro' ); ?></span><!-- single_test_descrip -->
+
+						<div class='single_test_content content'>
+			
+							<?php the_sub_field( 'description' ); ?>
+			
+						</div><!-- single_test_content -->
+
+						<span class='double_line'></span><!-- double_line -->
+
+						<span class='single_test_name'><?php the_sub_field( 'name' ); ?></span><!-- single_test_name -->
+		
+					</div><!-- single_test -->
+		
+				<?php endwhile; ?>
+
+			<?php endif; ?>
 
 		</div><!-- testimonials_wrapper -->
 		
