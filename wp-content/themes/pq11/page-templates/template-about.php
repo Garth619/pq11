933b004a4e2a4a828927dc4bea859383e23fa581
Garrett Cullen <Garth619@users.noreply.github.com>
2020-07-09 17:27:31 -0700

    push

--- wp-content/themes/pq11/page-templates/template-about.php
+++ wp-content/themes/pq11/page-templates/template-about.php
@@ -10,17 +10,56 @@ get_header(); ?>
 	
 		<div id='about_content_inner'>
 		
-			<h1 id='banner_title'><?php the_title();?></h1><!-- banner_title -->
+			<h1 class='about_title banner_title'><?php the_title();?></h1><!-- banner_title -->
+
+			<span class='double_line'></span><!-- double_line -->
+
+			<div id='about_intro' class="content">
+			
+				<p>Experienced Trial Attorneys With a Focus on Complex, Mass-Action, and Class-Action Litigation Attorney James White focuses his practice on representing clients in matters that greatly affect their lives and organizations in complex, mass-action, and class-action litigation. White Law PLLC’s experience in leading an experienced team of professionals and working with institutional and legal partners sets the firm apart from its competitors.</p>
+
+			</div><!-- about_intro -->
 		
 		</div><!-- about_content_inner -->
+
+		<img id='about_banner_image' src='<?php bloginfo('template_directory');?>/images/hero-intl-about-tablet-new.jpg' alt=''/><!-- about_banner_image -->
 	
 	</div><!-- about_banner -->
 
-	<div class="page_container">
+	<div class="page_container about_container two_col content">
 		
+		<div class='about_col'>
 		
+			<h2>Experienced Attorneys Serving Okemos, Lansing, Michigan & Nationwide</h2>
+
+			<p>White Law PLLC was founded to provide more personalized representation to clients facing legal dilemmas. Whether you have an issue that is complex or relatively straightforward, any legal matter can have a significant impact on your life. Our team of experienced lawyers based in Okemos, Michigan, strive to deliver outstanding legal services that exceed your expectations. We utilize our extensive legal knowledge and the latest in technology to bring you effective and proven solutions to your legal issues.</p>
+
+			<h2>Unique Qualities of Our Law Practice:</h2>
+
+			<h3>People Come First</h3>
+
+			<p>We put your need as our first priority. Those know who their attorney is from the start and work directly with the attorney through the entirety of the case. We take the time to get to know you, your issue, and find a tailored solution to meet your needs.</p>
+
+			<h3>Proven Experience & Solutions</h3>
+
+			<p>Members of our law firm bring their diverse legal backgrounds and extensive knowledge of the law to each case. We are poised to deliver exceptional solutions due to our experience handling a broad range of legal matters, both straightforward and complex.</p>
+
 		
-	</div><!-- page_container -->
+		</div><!-- about_col -->
+
+		<div class='about_col'>
+		
+			<h3>The Latest Technology</h3>
+
+			<p>We utilize the latest in new and emerging technologies by integrating them into our legal services. From iPads, iPhones, laptops, to cloud services, lawyers and staff are connected at all times. Clients can also conveniently discuss their cases remotely and access case files. Our use of cutting-edge technology is also utilized in the courtroom.</p>
+
+			<h3>Paperless Law Practice</h3>
+
+			<p>We have made the transition towards a paperless law practice to reduce unnecessary expenses and pass on our cost savings to our clients. In addition to reducing on these overhead costs, our paperless model enables a more efficient and reliable workflow and allows clients to access their case from any electronic device.</p>
+		
+		</div><!-- page_container -->
+		
+	</div><!-- about_container -->
 	
 </div><!-- internal_main -->
 		
