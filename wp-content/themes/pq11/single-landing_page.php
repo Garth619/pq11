b078ee4993a72aba68d23b8060e3278db4c1ecf7
Garrett Cullen <Garth619@users.noreply.github.com>
2020-07-28 14:09:33 -0700

    push

---
+++ wp-content/themes/pq11/single-landing_page.php
@@ -0,0 +1,35 @@
+<div id='landing_page_wrapper'>
+
+  <div id='landing_page_left'>
+
+    <?php get_header(); ?>
+
+    <div id='landing_page_content_wrapper'>
+    
+      
+    
+    </div><!-- landing_page_content_wrapper -->
+
+    <?php get_template_part('page-templates/includes/template','footer'); ?>
+   
+
+  </div><!-- landing_page_left -->
+
+  <div id='landing_page_sidebar'>
+  
+    
+  
+  </div><!-- landing_page_sidebar -->
+
+</div><!-- landing_page_wrapper -->
+
+
+
+<?php wp_footer();?>
+
+
+
+</body>
+</html>
+
+
