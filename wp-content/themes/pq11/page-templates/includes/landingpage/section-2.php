240c43d13a4787a5081ef5044979cd4bd73cae78
Garrett Cullen <Garth619@users.noreply.github.com>
2020-07-29 14:10:49 -0700

    push

--- wp-content/themes/pq11/page-templates/includes/landingpage/section-2.php
+++ wp-content/themes/pq11/page-templates/includes/landingpage/section-2.php
@@ -1,5 +1,17 @@
 <section id='lp_section_two'>
 
+  <div id='lp_sec_two_inner'>
   
+    <div id='lp_sec_two_slider'>
+    
+      <div class='lp_sec_two_slide'>
+      
+        
+      
+      </div><!-- lp_sec_two_slide -->
+    
+    </div><!-- lp_sec_two_slider -->
+  
+  </div><!-- lp_sec_two_inner -->
 
 </section><!-- lp_section_two -->
