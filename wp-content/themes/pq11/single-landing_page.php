9df23cb32a6c0670fe74f431f6fbe211e3b43f89
Garrett Cullen <Garth619@users.noreply.github.com>
2020-07-29 12:35:22 -0700

    push

--- wp-content/themes/pq11/single-landing_page.php
+++ wp-content/themes/pq11/single-landing_page.php
@@ -12,12 +12,25 @@
 
     <?php get_template_part('page-templates/includes/template','footer'); ?>
    
-
   </div><!-- landing_page_left -->
 
   <div id='landing_page_sidebar'>
-  
-    
+
+   <div id='landing_page_sidebar_inner'>
+   
+     <div class='sidebar_form'>
+ 
+       <span class='sidebar_form_title'>Request a free consultation</span><!-- sidebar_form_title -->
+ 
+       <span class='double_line'></span><!-- double_line -->
+ 
+       <?php gravity_form(5, false, false, false, '', true, 14433); ?>
+ 
+       <span class='required'>Required Field</span><!-- sidebar_required -->
+ 
+     </div><!-- sidebar_form -->
+   
+   </div><!-- landing_page_sidebar_inner -->
   
   </div><!-- landing_page_sidebar -->
 
